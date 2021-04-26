539ae6300bcfe04a63433b24b785c612a8afa845
TuPV <tupv@vnpt.vn>
2021-04-26 14:51:25 +0700

    update changes

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            this.discountProtoServiceClient = discountProtoServiceClient ?? throw new ArgumentNullException(nameof(discountProtoServiceClient));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            try
+            {
+                var discountRequest = new GetDiscountRequest { ProductName = productName };
+                return await discountProtoServiceClient.GetDiscountAsync(discountRequest);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
